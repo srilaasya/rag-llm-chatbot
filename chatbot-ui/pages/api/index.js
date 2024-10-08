fa59fa8f957f65a5bfbb2f7658576ec089f6d65c
Sri Laasya Nutheti <nutheti.laasya@gmail.com>
2024-10-08 15:20:05 -0700

    Add index.js for homepage

---
+++ chatbot-ui/pages/api/index.js
@@ -0,0 +1,12 @@
+// chatbot-ui/pages/index.js
+const Home = () => {
+    return (
+        <div>
+            <h1>Welcome to the Chatbot</h1>
+            <p>This is the homepage of the chatbot application.</p>
+            <a href="/chat">Go to Chat</a>
+        </div>
+    );
+};
+
+export default Home;
